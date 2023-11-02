6cfaf82a457ac1b408fc838ad3787eb77002aded
samuel-marinho <samuelmarinho.dev@gmail.com>
2023-11-02 11:07:04 -0300

    Lançamento de estoque

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        /*Esse exercício POO lê 3 caracteristicas de um produto e faz
+          o controle de estoque, adicionando ou removendo unidades
+          e mostrando os dados atualizados.
+         */
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
@@ -16,8 +21,23 @@ public class Program {
         product.price = sc.nextDouble();
         System.out.print("Quantity in stock: ");
         product.quantity = sc.nextInt();
+        System.out.println();
+        System.out.println("Product data: "+ product);
 
-        System.out.println(product);
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+
+        int quantity = sc.nextInt();
+        product.addProduct(quantity);
+        System.out.println();
+        System.out.println("Updated product data: "+ product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+        System.out.println();
+        System.out.println("Updated product data: "+ product);
 
         sc.close();
     }
