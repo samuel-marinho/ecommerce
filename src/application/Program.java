3eeee6af50291bafee458421f6b7d818caaabd86
samuel-marinho <samuelmarinho.dev@gmail.com>
2023-11-06 22:00:21 -0300

    Encapsulamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,14 +13,13 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
         System.out.println("Enter product data: ");
         System.out.println("Name: ");
-        product.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Price: ");
-        product.price = sc.nextDouble();
-        System.out.print("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+        Product product = new Product(name, price);
+
         System.out.println();
         System.out.println("Product data: "+ product);
 
